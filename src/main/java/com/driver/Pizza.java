874c1760c777ad99fa2fed80c84a7aecea1ca727
Praveen <praveenk9417@gmail.com>
2022-12-10 23:03:18 +0530

    Submit Pizza Billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,21 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int base, cheese, toppings, paperBag;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        cheese = 0;
+        toppings = 0;
+        paperBag = 0;
+        if(isVeg) {
+            base = 300;
+            price = base;
+        }
+        else {
+            base = 400;
+            price = base;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +27,46 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(cheese>0) return;
+        cheese += 80;
+        price += cheese;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(toppings>0) return;
+        if(isVeg) {
+            toppings += 70;
+            price += toppings;
+        }
+        else {
+            toppings += 120;
+            price += toppings;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(paperBag>0) return;
+        paperBag += 20;
+        price += paperBag;
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        String basePrice  = "Base Price Of The Pizza: "+base;
+        String cheesePrice = "Extra Cheese Added: "+cheese;
+        String toppingsPrice = "Extra Toppings Added: "+toppings;
+        String paperPrice = "Paperbag Added: "+paperBag;
+        String totalPrice = "Total Price: "+price;
+        if(cheese>0&&toppings>0&&paperBag>0) {
+            return basePrice+'\n'+cheesePrice+'\n'+toppingsPrice+'\n'+paperPrice+'\n'+totalPrice;
+        }
+        else if(cheese>0&&toppings>0) {
+            return basePrice+'\n'+cheesePrice+'\n'+toppingsPrice+'\n'+totalPrice;
+        }
+        else if(toppings>0&&paperBag>0) {
+            return basePrice+'\n'+toppingsPrice+'\n'+paperPrice+'\n'+totalPrice;
+        }
+        else {
+            return basePrice+'\n'+cheesePrice+'\n'+paperPrice+'\n'+totalPrice;
+        }
     }
 }
